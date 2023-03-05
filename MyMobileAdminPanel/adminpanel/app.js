c5ac996062ae43c2599052639ae5bb8e839632de
melaren12 <melaren6@gmail.com>
2023-03-05 20:24:45 +0400

    edit

--- MyMobileAdminPanel/adminpanel/app.js
+++ MyMobileAdminPanel/adminpanel/app.js
@@ -1,10 +1,16 @@
 const add = document.querySelector('.addbtn');
 const popup = document.querySelector('.popup');
-const exit = document.querySelector('.exit')
+const exit = document.querySelector('.exit');
+const edit = document.querySelector('.edit')
 
 add.addEventListener('click', (e) => {
     popup.classList.toggle('popup2');
 })
+
+edit.addEventListener('click', (e) => {
+    popup.classList.toggle('popup2');
+})
+
 
 exit.addEventListener('click', (e) => {
     popup.classList.toggle('popup2');
